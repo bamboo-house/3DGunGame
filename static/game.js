1ed97d86bd3e0d1270f120f60055834dc2992450
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2022-12-18 18:06:47 +0900

    エラ対処中

--- static/game.js
+++ static/game.js
@@ -1,19 +1,14 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const socket_io_client_1 = require("socket.io-client");
-const jquery_1 = __importDefault(require("jquery"));
-const socket = (0, socket_io_client_1.io)();
+import { io } from "socket.io-client";
+import $ from 'jquery';
+const socket = io();
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
-const playerImage = (0, jquery_1.default)('#player-image')[0];
+const playerImage = $('#player-image')[0];
 let movement = {};
 const gameStart = () => {
     socket.emit('game-start');
 };
-(0, jquery_1.default)(document).on('keydown keyup', (event) => {
+$(document).on('keydown keyup', (event) => {
     const KeyToCommand = {
         'ArrowUp': 'forward',
         'ArrowDown': 'back',
