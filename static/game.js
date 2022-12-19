8eaa4a4dadff8dfea328d6b68627a74adf86a7ef
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2022-12-19 19:19:34 +0900

    jsに変更する

--- static/game.js
+++ static/game.js
@@ -1,13 +1,16 @@
-import { io } from "socket.io-client";
-import $ from 'jquery';
+'use strict';
+
 const socket = io();
-const canvas = document.getElementById('canvas');
+const canvas = $('#canvas-2d')[0];
 const context = canvas.getContext('2d');
 const playerImage = $('#player-image')[0];
 let movement = {};
-const gameStart = () => {
+
+
+function gameStart(){
     socket.emit('game-start');
-};
+}
+
 $(document).on('keydown keyup', (event) => {
     const KeyToCommand = {
         'ArrowUp': 'forward',
@@ -16,26 +19,29 @@ $(document).on('keydown keyup', (event) => {
         'ArrowRight': 'right',
     };
     const command = KeyToCommand[event.key];
-    if (command) {
-        if (event.type === 'keydown') {
+    if(command){
+        if(event.type === 'keydown'){
             movement[command] = true;
-        }
-        else {
+        }else{ /* keyup */
             movement[command] = false;
         }
         socket.emit('movement', movement);
     }
 });
+
 socket.on('state', (players, bullets, walls) => {
     context.clearRect(0, 0, canvas.width, canvas.height);
+
     context.lineWidth = 10;
     context.beginPath();
     context.rect(0, 0, canvas.width, canvas.height);
     context.stroke();
+
     Object.values(players).forEach((player) => {
-        context.drowImage(playerImage, player.x, player.y);
+        context.drawImage(playerImage, player.x, player.y);
         context.font = '30px Bold Arial';
         context.fillText('Player', player.x, player.y - 20);
     });
 });
+
 socket.on('connect', gameStart);
