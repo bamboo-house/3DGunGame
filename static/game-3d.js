613aa5dec77d434cfebbd331176fdd823ce7a195
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2022-12-24 17:10:59 +0900

    3dクライアント用意

---
+++ static/game-3d.js
@@ -0,0 +1,5 @@
+const socket = io();
+const canvas2d = $('#canvas-2d')[0];
+const context = canvas2d.getContext('2d');
+const canvas3d = $('#canvas-3d')[0];
+const playerImage = $("#player-image")[0];
